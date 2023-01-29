47d05d49078178ef9e88dd87e46478ea51c61a72
fraagu10 <aguilarfranco1092@protonmail.com>
2023-01-29 12:03:06 -0600

    walkthrough completed

--- lesson3/examples/src/app/buttons/buttons.component.ts
+++ lesson3/examples/src/app/buttons/buttons.component.ts
@@ -6,11 +6,20 @@ import { Component, OnInit } from '@angular/core';
    styleUrls: ['./buttons.component.css']
 })
 export class ButtonsComponent implements OnInit {
+
    buttonHeading: string = "Buttons"
    inactive: boolean = false;
+   inactiveS: boolean = false;
+   inactiveC: boolean = false;
 
    constructor() { }
 
    ngOnInit() { }
+
+   resetButtons(): void {
+      this.inactive = false;
+      this.inactiveC = false;
+      this.inactiveS = false;
+   }
 
 }
