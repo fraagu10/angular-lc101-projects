b6c350d731d24231f0dccbf33b2266bd858e3faa
fraagu10 <aguilarfranco1092@protonmail.com>
2023-03-09 17:40:55 -0600

    added toolbar and angular material

--- lesson3/exercises/src/app/app.module.ts
+++ lesson3/exercises/src/app/app.module.ts
@@ -2,13 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlightSimulatorComponent } from './flight-simulator/flight-simulator.component';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
     AppComponent,
+    FlightSimulatorComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
